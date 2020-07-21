ddd8b718ab49d33f2c8e602a429b850637498ceb
Gurprem Bhullar <bhullargurprem@gmail.com>
2020-07-21 12:24:15 -0400

    my first commit

--- development/homework/Employee-Summary/Develop/lib/Employee.js
+++ development/homework/Employee-Summary/Develop/lib/Employee.js
@@ -1,9 +1,29 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor(name){
+    constructor(name, id, email, role) {
         this.name = name
+        this.id = id
+        this.email = email
+        this.role = role
+    }
+    getRole() {
+        return this.role;
+
     }
     
+    getName(){  
+        return this.name;
+
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
 }
 
 module.exports = Employee
--- development/homework/Employee-Summary/Develop/lib/Engineer.js
+++ development/homework/Employee-Summary/Develop/lib/Engineer.js
@@ -1,1 +1,11 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require ("../lib/Employee.js");
+// TO DO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Engineer extends Employee {
+    constructor(name, id, email, github, role){
+        super(name, id, email, github, role);
+        this.github = github;
+    }
+}
+
+module.exports = Engineer
