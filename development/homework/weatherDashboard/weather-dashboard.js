d07d3e17ddbf949a871a1a5333bd0438d60cd872
Gurprem Bhullar <bhullargurprem@gmail.com>
2020-07-24 17:54:16 -0400

    added some features

---
+++ development/homework/weatherDashboard/weather-dashboard.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var current_Date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var apiKey = "28e8f3efb740c29dfea71effd97c7ec1";
+    var differentCities = [];
+    var cityInput = $("#city-input-form").val().trim();
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey; 
+
+    $("#location-search-button").on("click", function() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+          .then(function (response){
+        console.log(response);
+        
+        $("#locationName").text(response.name); 
+        $("#locationDate").text(current_Date);  
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed);
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32; 
+        $("#temperature").text("Temperature (F) " + tempF.toFixed(2));
+
+          }) 
+     }) 
+})
+
+   
