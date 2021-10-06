5d226aa1f9208bfaa2b0ea836209eb39928b7f71
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-06 16:13:27 -0400

    imported src into new react folder

---
+++ development/reactlinkedinclone/src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>this is a header</h1>
+
+            <div className="header__left">
+                <img src="" alt=""/>
+            </div>
+
+            <div className="header__search">
+                <SearchIcon />
+            </div>
+
+            <div className="header__right">
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
