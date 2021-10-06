73a2774e972e16a1bbb948fcbf74dda45b7878c7
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-06 07:45:15 -0400

    created template for app.js

--- development/reactAppLinkedinClone/reactapplinkedinclone/src/App.js
+++ development/reactAppLinkedinClone/reactapplinkedinclone/src/App.js
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build a LinkedIn Clone!</h1>
+
+      {/*Header */}
+
+      {/*App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
       
     </div>
   )
 }
+
+export default App;
