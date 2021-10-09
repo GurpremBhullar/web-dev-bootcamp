1418fb1b8d8e2e7ee7fd80f2542c0b3ef02123c2
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-09 19:14:30 -0400

    added files and stylesheets

--- development/reactlinkedinclone/src/Header.js
+++ development/reactlinkedinclone/src/Header.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
+import linkedinPic from './linkedin.jpg';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 function Header() {
     return (
         <div className='header'>
-            <h1>this is a header</h1>
+            
 
             <div className="header__left">
-                <img src="" alt=""/>
+                <img src={linkedinPic} alt=" "/>
             </div>
 
             <div className="header__search">
                 <SearchIcon />
+                <input type="text" />
+                
             </div>
 
             <div className="header__right">
-
-
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
             </div>
         </div>
     )
