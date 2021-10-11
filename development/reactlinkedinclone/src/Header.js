785563593667067e53b36deacd28cb0dd4b09d91
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-11 15:59:31 -0400

    added a avatar using material UI and Icons

--- development/reactlinkedinclone/src/Header.js
+++ development/reactlinkedinclone/src/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Header.css";
 import Linkedin from "./linkedin.jpg"
+import Gurprem from "./gurprem.jpg"
 import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Avatar } from '@mui/material';
+
 
 function Header() {
     return (
@@ -25,7 +31,11 @@ function Header() {
 
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title="Home" />
-                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="SupervisorAccount" />
+                <HeaderOption Icon={BusinessCenterIcon} title="BusinessCenter" />
+                <HeaderOption Icon={ChatIcon} title="Chat" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <Avatar alt="gurprem" src={Gurprem} />
             </div>
         </div>
     )
